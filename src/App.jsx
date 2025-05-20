7345a6f53407d0d9d97571349379f5c43bd8aaa5
TariqNew <marandumashaka78@gmail.com>
2025-05-21 02:36:47 +0300

    chore: remove routes from the App

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import Home from './Pages/Home/Home';
-import About from './Pages/About/About';
-import Services from './Pages/Services/Services';
+import "./App.css";
+import Home from "./Pages/Home/Home";
+import About from "./Pages/About/About";
+import Services from "./Pages/Services/Services";
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/services" element={<Services />} />
-      </Routes>
+      <Home />
+      <About />
+      <Services />
     </>
   );
 }
