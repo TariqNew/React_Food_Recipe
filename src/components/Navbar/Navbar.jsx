33f86fdff5ac349c2edc425d83b95b24727144e6
TariqNew <marandumashaka78@gmail.com>
2025-05-20 11:51:58 +0300

    feat(Button): Adding button component to the project

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,44 @@
+import { Link } from "react-router-dom";
+import foodImage from "../../assets/images/food.jpg";
+import Button from "../Button/Button";
+
+export default function Navbar() {
+  return (
+    <nav className="text-white font-large p-4">
+      <div className="container mx-auto flex items-center justify-between backdrop-blur-md bg-white/30 rounded-md p-4">
+        <div className="flex items-center space-x-3">
+          <img
+            src={foodImage}
+            alt="Food"
+            className="h-10 w-10 rounded-full object-cover"
+          />
+          <span className="text-xl font-bold">FoodRecipe</span>
+        </div>
+        
+        {/* Navigation Links */}
+        <div className="flex space-x-6">
+          <Link
+            to="/"
+            className="transition duration-300 px-3 py-2 rounded-md font-medium"
+          >
+            Home
+          </Link>
+          <Link
+            to="/about"
+            className="transition duration-300 px-3 py-2 rounded-md font-medium"
+          >
+            About
+          </Link>
+          <Link
+            to="/services"
+            className="transition duration-300 px-3 py-2 rounded-md font-medium"
+          >
+            Services
+          </Link>
+          <Button name={'Get Started'}/>
+        </div>
+        
+      </div>
+    </nav>
+  );
+}
