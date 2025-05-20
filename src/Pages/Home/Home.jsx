71c9ce4d3b55827cbe5d63cf8da8f1b5aa409c58
TariqNew <marandumashaka78@gmail.com>
2025-05-20 07:19:58 +0300

    feat(style): Adding styles to the pages

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import './Home.css';
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <div className="home-content">
+        <h1>Home Page</h1>
+      </div>
+    </div>
+  );
+}
