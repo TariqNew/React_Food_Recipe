371ce35f1a6aebca5684c25265985da80afa751e
TariqNew <marandumashaka78@gmail.com>
2025-05-20 11:54:35 +0300

    refactor(Home):Updating Home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,28 @@
-import './Home.css';
+import "./Home.css";
+import Navbar from "../../components/Navbar/Navbar";
+import Button from "../../components/Button/Button";
+import About from "../About/About";
 
 export default function Home() {
   return (
-    <div className="home-container">
-      <div className="home-content">
-        <h1>Home Page</h1>
+    <>
+      <div className="home-section">
+        <Navbar />
+        <div className="home-container">
+          <div className="home-content">
+            <h1>
+              Welcome to <span>FoodRecipe</span>
+            </h1>
+            <p className="home-subtitle">
+              Discover delicious recipes, explore diverse cuisines, and cook like a pro from the <br />comfort of your kitchen.
+            </p>
+            <div className="home-buttons">
+              <Button name={'Explore Recipes'}/>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+      <About />
+    </>
   );
 }
