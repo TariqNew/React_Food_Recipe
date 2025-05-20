8746e864b7b25f8a2765b21b6c89b038fa66b4de
TariqNew <marandumashaka78@gmail.com>
2025-05-20 12:08:46 +0300

    refactor(App): Adding features to the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import './App.css'
-import { Routes, Route } from 'react-router-dom'
-import Home from './Pages/Home'
-import About from './Pages/About'
-import Services from './Pages/Services'
-import Navbar from './components/navbar/Navbar'
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
+import Services from './Pages/Services/Services';
 
 function App() {
   return (
     <>
-      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
