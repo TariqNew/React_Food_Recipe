bdd1c1c795036f559444d2f87ebfc27ad94bfe47
TariqNew <marandumashaka78@gmail.com>
2025-05-20 17:48:09 +0300

    chore: Reomve navigation bar at the App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar/Navbar';
 import Home from './Pages/Home/Home';
 import About from './Pages/About/About';
 import Services from './Pages/Services/Services';
